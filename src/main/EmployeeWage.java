38adbb34b8537f0746da41f4c585022bd3fda310
Sunny996 <satlaakash@gmail.com>
2022-04-05 00:07:36 +0530

    ADDED: A method to calculate Employee Wage Per Day

--- src/main/EmployeeWage.java
+++ src/main/EmployeeWage.java
@@ -5,14 +5,17 @@ public class EmployeeWage {
     public final int IS_ABSENT=0;
     public final int WAGE_PER_HOUR=20;
     public final int FULL_DAY_HOUR=8;
+    public final int PART_TIME_HOUR = 4;
+    public final int IS_PART_TIME = 2;
 
     public int empDailyWage(){
         int dailyWage=0;
-        int empPresent=(int)(Math.random()*10)%2;
+        int empPresent=(int)(Math.random()*10)%3;
         if(empPresent==IS_PRESENT)
             dailyWage=WAGE_PER_HOUR*FULL_DAY_HOUR;
-        System.out.println("Daily Wage on a Full Day is "+dailyWage);
-        System.out.println(empPresent);
+        else if(empPresent==IS_PART_TIME)
+            dailyWage=WAGE_PER_HOUR*PART_TIME_HOUR;
+        System.out.println("Daily Wage is "+dailyWage);
         return dailyWage;
     }
 }
