198920109dd406371112debcfa582214e5cb0d6c
Akash Satla <65150847+Sunny996@users.noreply.github.com>
2022-04-05 00:48:54 +0530

    Update EmployeeWage.java

--- src/main/EmployeeWage.java
+++ src/main/EmployeeWage.java
@@ -27,7 +27,7 @@ public class EmployeeWage {
 
             }
         }
-        System.out.println("Employee Wage Per Month in 100 hours or 20 days for "+company+" is " + totalWage);
+        System.out.println("Employee Wage Per Month for "+company+" is " + totalWage);
         System.out.println("Days " + totalWorkingDays);
         System.out.println("Hours " + totalWorkingHours);
         return totalWage;
